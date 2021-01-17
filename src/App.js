0c75e08f7a859ede527d5acba3da4df52ba43683
NatrezC <cnatrez@gmail.com>
2021-01-16 20:21:45 -0600

    added some style to my app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Button from '@material-ui/core/Button'
 import Introduction from './components/Intro/Introduction'
 import About from './components/About/About';
-import Projects from './components/Projects';
+import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Introduction />
       <About />
       <Projects />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <div className="container">
+                <div className="header">
+                    Natrez's Portfolio
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
