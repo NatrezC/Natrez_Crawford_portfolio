c953ac99fd62d02f0e0bfc37b72300fcb6efa585
NatrezC <cnatrez@gmail.com>
2021-01-16 22:33:41 -0600

    underline under the link when you are in that section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Introduction from './components/Intro/Introduction'
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
-import Header from './components/Header/Navbar';
+import Header from './components/Navbar/Navbar';
 
 
 function App() {
