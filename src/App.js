f68d9f2748b376e70e395bd28d28efbe51986829
NatrezC <cnatrez@gmail.com>
2021-01-16 19:56:53 -0600

    created my components for the site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import Introduction from './components/Intro/Introduction'
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Introduction />
+      <About />
+      <Projects />
+      <Contact />
+      {/* <Footer /> */}
     </div>
   );
 }
