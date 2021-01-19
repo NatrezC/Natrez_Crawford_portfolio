cd72110d8aae16fa295e42c60d345ed578affc61
NatrezC <cnatrez@gmail.com>
2021-01-18 21:03:34 -0600

    trying to add an avatar

--- src/components/Intro/Introduction.js
+++ src/components/Intro/Introduction.js
@@ -1,27 +1,63 @@
+import { LocalDining } from '@material-ui/icons';
 import React, { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-import Wobble from 'react-reveal/Wobble';
-import './Introduction.css'
+import { Grid, Cell } from 'react-mdl';
 
-class Introduction extends React.Component {
-    state={}
+class Introduction extends Component{
     render() {
         return (
-            <div>
-                    <div className="main-section" id="intro">
-                        <Fade left >
-                        <h1><strong>Hi! My name is <span>Natrez Crawford</span></strong></h1>
-                        <h2><strong>and I am a <span>Full Stack Software Engineer</span></strong></h2>
+            <div className="main-section">
+                <div style={{width: '100%', margin: 'auto'}}>
+                    <Grid className="intro-grid">
+                        <Cell col={12}>
+                            <img
+                                src="/images/avatar.png"
+                                alt="avatar"
+                                className="avatar-img"
+                            />
                             
-                        </Fade>
-                        
-                    </div>
-                
+                        </Cell>
+                    </Grid>
+                </div>
             </div>
-        );
+        )
     }
 }
 
 export default Introduction;
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import Fade from 'react-reveal/Fade';
+// import Wobble from 'react-reveal/Wobble';
+// import './Introduction.css'
+
+// class Introduction extends React.Component {
+//     state={}
+//     render() {
+//         return (
+//             <div>
+//                     <div className="main-section" id="intro">
+//                         <Fade left >
+//                         <h1><strong>Hi! My name is <span>Natrez Crawford</span></strong></h1>
+//                         <h2><strong>and I am a <span>Full Stack Software Engineer</span></strong></h2>
+                            
+//                         </Fade>
+                        
+//                     </div>
+                
+//             </div>
+//         );
+//     }
+// }
+
+// export default Introduction;
 
 
