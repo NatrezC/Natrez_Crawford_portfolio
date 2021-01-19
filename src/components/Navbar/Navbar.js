f3589b7661383301965ff072079072586b808c49
NatrezC <cnatrez@gmail.com>
2021-01-18 23:01:45 -0600

    remaking navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react'
-import { render } from '@testing-library/react';
+import React, { useState } from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll'
-import './Navbar.css'
-import Button from '@material-ui/core/Button'
 
-class Header extends Component {
-    
-    // scrollToTop = () => {
-    //     scroll.scrollToTop()
-    // }
-    render(){
-        return (
-            <div className='navbar'>
-                <div className="links">
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
                     <div className="list">
                         <Link
                             activeClass="active"
@@ -21,43 +15,86 @@ class Header extends Component {
                             smooth={true}
                             offset={-30}
                             duration={1000}
-                        >About</Link>
+                        ><span><strong>Natrez Crawford</strong></span></Link>
                     </div>
-                    <div className="list">
-                        <Link
-                            activeClass="active"
-                            to="projects"
-                            spy={true}
-                            smooth={true}
-                            offset={-30}
-                            duration={1000}
-                        >Projects</Link>
-                    </div>
-                    <div className="list">
-                        <Link
-                            activeClass="active"
-                            to="contact"
-                            spy={true}
-                            smooth={true}
-                            offset={-30}
-                            duration={1000}
-                        >Contact</Link>
-                    </div>
-                    
                 </div>
-                {/* <div className="toTheTop" id ="myID">
-                    <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={this.scrollToTop}
-                    disableElevation
-                >SCROLL TO TOP
-                </Button>
-                </div> */}
-                
-            </div>
-        )
-    }
+            </nav>
+        </>
+    )
 }
 
-export default Header
+export default Navbar
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react'
+// import { render } from '@testing-library/react';
+// import { Link, animateScroll as scroll } from 'react-scroll'
+// import './Navbar.css'
+// import Button from '@material-ui/core/Button'
+
+// class Navbar extends Component {
+    
+//     scrollToTop = () => {
+//         scroll.scrollToTop()
+//     }
+//     render(){
+//         return (
+//             <div className='navbar' >
+//                 <div className="title" id="myID">
+//                     <h3 onClick={this.scrollToTop}>
+//                         <strong>Natrez Crawford</strong>
+//                     </h3>
+//                     {/* <Button
+//                         variant="contained"
+//                         color="primary"
+//                         onClick={this.scrollToTop}
+//                         disableElevation
+//                     >SCROLL TO TOP
+//                 </Button> */}
+//                 </div>
+//                 <div className="links">
+//                     <div className="list">
+//                         <Link
+//                             activeClass="active"
+//                             to="about"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-30}
+//                             duration={1000}
+//                         ><span><strong>About</strong></span></Link>
+//                     </div>
+//                     <div className="list">
+//                         <Link
+//                             activeClass="active"
+//                             to="projects"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-30}
+//                             duration={1000}
+//                         ><span><strong>Projects</strong></span></Link>
+//                     </div>
+//                     <div className="list">
+//                         <Link
+//                             activeClass="active"
+//                             to="contact"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-30}
+//                             duration={1000}
+//                         ><span><strong>Contact</strong></span></Link>
+//                     </div>
+                    
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Navbar
+
